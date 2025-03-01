df1334e96beee8e465604d221767bd85696eb16f
Leonardo Veras <andradeveras96@gmail.com>
2025-03-01 13:00:21 -0300

    Add create method

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,21 @@
 class TasksController < ApplicationController
   def index
+    @task = Task.new
     @tasks = Task.all
-    @tasks = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: 'Task was successfully created.'
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:description)
   end
 end
