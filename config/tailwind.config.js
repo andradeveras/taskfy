ba5f40bd4036f03caefb89415276da163671a444
Leonardo Veras <andradeveras96@gmail.com>
2025-02-28 23:48:54 -0300

    Config Tailwind

---
+++ config/tailwind.config.js
@@ -0,0 +1,10 @@
+export const content = [
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*',
+];
+export const theme = {
+    extend: {},
+};
+export const plugins = [];
+  
